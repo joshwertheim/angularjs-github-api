58754c25391c99e8180d88a2640479f3243c2db3
Joshua Wertheim <joshwertheim@gmail.com>
2015-03-28 16:46:01 -0700

    API url fix. Now uses API pagination to only pull and show ten most recent commits.

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var gitApp = angular.module('gitApp',[]);
 gitApp.controller('rootController', ['$window', '$scope', '$http', function($window, $scope, $http) {
 
 	$scope.submit = function() {
-		$scope.apiUrl = "https://api.github.com/repos/" + $scope.owner + "/" + $scope.repo + "/commits";
+		$scope.apiUrl = "https://api.github.com/repos/" + $scope.owner + "/" + $scope.repo + "/commits?page=1&per_page=10";
         // $window.alert($scope.apiUrl);
 
         $http.get($scope.apiUrl).success(function(data) {

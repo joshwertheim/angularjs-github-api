ece3642cdd98dc52d128c85ba204ce9eebbf6674
Joshua Wertheim <joshwertheim@gmail.com>
2015-03-28 19:12:23 -0700

    Update app.js and index.html Added a <small> element to show results of algorithm for checking which contributor in recent 10 commits had the most contributions in the period.

--- app.js
+++ app.js
@@ -2,13 +2,34 @@
 
 var gitApp = angular.module('gitApp',[]);
 
-gitApp.controller('rootController', ['$window', '$scope', '$http', function($window, $scope, $http) {
+gitApp.controller('rootController', ['$window', '$scope', '$log', '$http', function($window, $scope, $log, $http) {
+
+	$scope.commits = {};
 
 	$scope.submit = function() {
 		$scope.apiUrl = "https://api.github.com/repos/" + $scope.owner + "/" + $scope.repo + "/commits?page=1&per_page=10";
 
         $http.get($scope.apiUrl).success(function(data) {
         	$scope.commits = data;
+
+        	var arrayLength = $scope.commits.length;
+        	var authors = {};
+        	var max = 0;
+        	$scope.most = '';
+
+        	for (var i = 0; i < arrayLength; i++) {
+        		var name = $scope.commits[i].commit.author.name;
+
+        	    if(name in authors) {
+        	    	authors[name] = authors[name] + 1;
+        	    	if(authors[name] > max) {
+        	    		max = authors[name];
+        	    		$scope.most = name;
+        	    	}
+        	    } else {
+        	    	authors[name] = 0;
+        	    }
+        	}
         });
     };
 	
